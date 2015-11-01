d28a0612ba3b03dc642c7eaad13c6c9dc4f94a47
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-01 15:10:51 -0600

    jf: kill thermal-engine
    * use in-kernel solution
    
    Change-Id: If04e1294a6085178dd89ae7fba15a33665f97823
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- jf-common.mk
+++ jf-common.mk
@@ -195,11 +195,6 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-8064ab.conf:system/etc/thermal-engine-8064ab.conf
-
 # USB
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
